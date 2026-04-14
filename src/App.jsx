570f23763a771fc5cbd4048fd52ccd219ef562ba
KhadzA <alimkhadz111@gmail.com>
2026-04-14 21:59:22 +0800

    Head

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,45 @@ function App() {
     <div className="content">
 
       <header className="header">
-        Alimun
+        <div className="primary-HeadTitle">
+          <img src="../react-logo.svg" alt="primary-logo" />
+          Alimun
+        </div>
 
-        <nav>
-          <ul>
-            <li>
-              <a href="#home"><FaHome /> Home</a>
-            </li>
-            <li>
-              <a href="#about"><FaUser /> About</a>
-            </li>
-            <li>
-              <a href="#projects"><FaCode /> Projects</a>
-            </li>
-            <li>
-              <a href="#contribution"><FaGithub /> Contribution</a>
-            </li>
-            <li>
-              <a href="#education"><FaUserGraduate /> Education</a>
-            </li>
-            <li>
-              <a href="#contact"><FaPhone /> Contact</a>
-            </li>
-          </ul>
-        </nav>
+        <div className="navigationHeader">
+          <nav>
+            <ul>
+              <li>
+                <a href="#home"><FaHome /> Home</a>
+              </li>
+              <li>
+                <a href="#about"><FaUser /> About</a>
+              </li>
+              <li>
+                <a href="#projects"><FaCode /> Projects</a>
+              </li>
+              <li>
+                <a href="#contribution"><FaGithub /> Contribution</a>
+              </li>
+              <li>
+                <a href="#education"><FaUserGraduate /> Education</a>
+              </li>
+              <li>
+                <a href="#contact"><FaPhone /> Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
       </header>
 
       <section id="home">
-        <h1>Home Section</h1>
+        <p>Welcome to my portfolio</p>
+        <h1>Al-khazri Alim</h1>
+        <h3>Frontend, React, PHP, Node.js</h3>
+        <div className="quote">
+          Why is 6 afraid of 7? Because 7 8 9! I'm a developer passionate about building engaging and user-friendly experiences.
+        </div>
       </section>
       <section id="about">
         <h1>About Section</h1>
