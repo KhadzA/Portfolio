23cb455425d24c8d8de0973d32cda33e57cd8084
KhadzA <alimkhadz111@gmail.com>
2026-04-15 17:14:46 +0800

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 
 import './App.css'
-import { FaHome, FaUser, FaCode, FaGithub, FaUserGraduate, FaPhone } from "react-icons/fa"
+import { FaHome, FaUser, FaCode, FaGithub, FaUserGraduate, FaPhone, FaHtml5, FaCss3Alt, FaReact, FaPhp, FaNode, FaGitAlt, FaQuestion, FaRegEye } from "react-icons/fa"
+import { BsJavascript } from "react-icons/bs";
+import { SiMysql, SiVite } from "react-icons/si";
+import { RiSupabaseFill, RiSupabaseLine, RiFirebaseFill, RiFirebaseLine } from "react-icons/ri";
+import { VscVscode } from "react-icons/vsc";
+import { FaHandshake } from "react-icons/fa6";
+
+
 
 function App() {
 
@@ -20,7 +27,7 @@ function App() {
                 <a href="#home"><FaHome /> Home</a>
               </li>
               <li>
-                <a href="#about"><FaUser /> About</a>
+                <a href="#skills"><FaUser /> Skills</a>
               </li>
               <li>
                 <a href="#projects"><FaCode /> Projects</a>
@@ -48,11 +55,68 @@ function App() {
           Why is 6 afraid of 7? Because 7 8 9! I'm a developer passionate about building engaging and user-friendly experiences.
         </div>
       </section>
-      <section id="about">
-        <h1>About Section</h1>
+      <section id="skills">
+        <h1>Skills</h1>
+        <div className="skillCard">
+
+          <h3>Frontend</h3>
+          <div className="">
+            <span><FaHtml5 />HTML</span>
+            <span><FaCss3Alt />CSS</span>
+            <span><BsJavascript />JavaScript</span>
+            <span><FaReact />React</span>
+            <div className="proficiency">
+              <p>Proficiency: <span>Intermediate</span></p>
+            </div>
+          </div>
+        </div>
+        <div className="skillCard">
+          <h3>Backend</h3>
+          <div>
+            <span><FaPhp />PHP</span>
+            <span><FaNode />Node.js</span>
+            <div className="proficiency">
+              <p>Proficiency: <span>Intermediate</span></p>
+            </div>
+          </div>
+        </div>
+        <div className="skillCard">
+          <h3>Database</h3>
+          <div>
+            <span><SiMysql />MySQL</span>
+            <span><RiSupabaseFill />Supabase</span>
+            <span><RiFirebaseFill />Firebase</span>
+            <div className="proficiency">
+              <p>Proficiency: <span>Intermediate</span></p>
+            </div>
+          </div>
+        </div>
+        <div className="skillCard">
+          <h3>Tools & Technologies</h3>
+          <div>
+            <span><FaGitAlt />Git</span>
+            <span><FaGithub />Github</span>
+            <span><VscVscode />VSCode</span>
+            <span><SiVite />Vite</span>
+
+            <div className="proficiency">
+              <p>Proficiency: <span>Intermediate</span></p>
+            </div>
+          </div>
+        </div>
+        <div className="skillCard">
+          <h3>Others</h3>
+          <div>
+            <span><FaHandshake />Collaborative Development</span>
+            <span><FaRegEye />Learner</span>
+            <span><FaQuestion />Curiosity-driven Learning</span>
+
+          </div>
+        </div>
       </section>
       <section id="projects">
         <h1>Projects Section</h1>
+
       </section>
       <section id="contribution">
         <h1>Contribution Section</h1>
